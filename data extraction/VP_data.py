064162c85446982c37c43a758bbc657747fb6d09
Yashashvi <73185331+YashasviTrivedi@users.noreply.github.com>
2024-12-27 15:18:14 +0530

    Update VP_data.py

--- data extraction/VP_data.py
+++ data extraction/VP_data.py
@@ -5,34 +5,21 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
-# Set up Chrome options
 chrome_options = Options()
 chrome_options.add_argument("--headless")  # Run in headless mode (no GUI)
 chrome_options.add_argument("--no-sandbox")  # Required in some CI environments
 chrome_options.add_argument("--disable-dev-shm-usage")  # Handle shared memory issues
 
-# Set up WebDriver
-# Use the ChromeDriver from the system path
 service = Service("/usr/local/bin/chromedriver-linux64/chromedriver")
-
-# Initialize the WebDriver
 driver = webdriver.Chrome(service=service, options=chrome_options)
 
-#driver_path = "C:/Users/Yashashvi/Downloads/chromedriver-win64/chromedriver-win64/chromedriver.exe"  # Replace with the correct path
-#service = Service(driver_path)
-#driver = webdriver.Chrome(service=service)
-
-# Open CSV file to store the data
 with open('VP_data.csv', mode='a', newline='', encoding='utf-8') as file:
     writer = csv.writer(file)
 
     try:
         # Open the Vidyut Pravah website
         driver.get("https://vidyutpravah.in/")
-
-        with open("page_source.html", "w", encoding="utf-8") as file:
-            file.write(driver.page_source)
-
+        driver.save_screenshot("debug_screenshot.png")
 
         # Extract the data
         surplus_power = driver.find_element(By.XPATH, '//*[@id="spanAllIndiaSurplus"]').text
